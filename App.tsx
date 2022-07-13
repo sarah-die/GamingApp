985d5d97308bc010f4684477cda6ba7d3492bc41
ngrippa <nemo.grippa@dainst.de>
2022-07-13 18:48:43 +0200

    New feature: clicks are counted and limited. Refresh possible

--- App.tsx
+++ App.tsx
@@ -1,32 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View} from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 import { Button } from "./Elemente/Button";
+import { useState } from "react";
+
+const generalPlacement = (random: boolean) => {
+  return StyleSheet.create({
+    initialPosition: {
+      position: "absolute",
+      top: random ? Math.random() * 100 + "%" : "50%",
+      left: random ? Math.random() * 100 + "%" : "50%",
+      transform: [{ translateX: -50 }, { translateY: -50 }],
+    },
+  }).initialPosition;
+};
+
 // noinspection JSUnusedGlobalSymbols
 export default function App() {
+  const [currentPlacement, setCurrentPlacement] = useState<
+    ReturnType<typeof generalPlacement>
+  >(generalPlacement(false));
+
+  const [currentClick, setCurrentClick] = useState<number>(0);
+
+  const moveButton = () => {
+    setCurrentPlacement(generalPlacement(true));
+    setCurrentClick(currentClick + 1);
+  };
+
+  const refresh = () => {
+  setCurrentClick(0)
+    setCurrentPlacement(generalPlacement(false))
+  }
+
+  //x === 3 ? 'Hallo' : 'Falsch'
+
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hallo Sarah 2!</Text>
-      <Button text={"Click me!"} action={() => {}} />
+    <View style={currentClick === 11 ? generalPlacement(false) : currentPlacement}>
+      {currentClick < 11 ? (
+        <Button
+          text={currentClick ? "Nummer " + currentClick : "Klicke zum Starten"}
+          action={moveButton}
+        />
+      ) : (
+          <>
+        <Text>Super!</Text>
+          <Button text={"Refresh"} action={refresh}/>
+              </>
+      )}
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    fontWeight: 'normal',
-    fontStyle: 'italic',
-    color: 'blue'
-  },
-
-  button: {
-
-  }
-
-});
