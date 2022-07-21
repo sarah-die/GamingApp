f97be28d090246fe5b3ea138cf35c85d08642c61
ngrippa <nemo.grippa@dainst.de>
2022-07-21 14:08:44 +0200

    new functions: the winner is determined note: missing: show the players who has won

--- Games/TicTacToe.tsx
+++ Games/TicTacToe.tsx
@@ -45,19 +45,71 @@ const styles = StyleSheet.create({
 
 type Status = "" | "X" | "O";
 
+const startInstruction = "X begins. Choose a field to start the game.";
+const oInstruction = "O's turn. Choose a field.";
+const xInstruction = "X's turn. Choose a field.";
+const endInstruction = "The End.";
+const winConditions: number[][] = [
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
 export const TicTacToe = () => {
   const [currentStatus, setStatus] = useState<Status[]>(new Array(9).fill(""));
+  const [currentInstruction, setInstruction] =
+    useState<string>(startInstruction);
 
   const refreshField = (index: number) => {
     if (currentStatus[index] !== "") return;
     // if (currentStatus[index]) return;
     const newStatus = [...currentStatus];
+    const [player, numberOfEmptyFields] = getCurrentPlayer(newStatus);
+    newStatus[index] = player;
+    setStatus(newStatus);
+    const playersInstruction: string =
+      numberOfEmptyFields % 2 ? oInstruction : xInstruction;
+    setInstruction(playersInstruction);
+    // if (numberOfEmptyFields === 1) displayEnd();
+    displayEnd(newStatus);
+  };
+
+  const getCurrentPlayer = (newStatus: Status[]): [Status, number] => {
     const numberOfEmptyFields = newStatus.filter((el) => {
       return el === "";
     }).length;
     const player: Status = numberOfEmptyFields % 2 ? "X" : "O";
-    newStatus[index] = player;
+    return [player, numberOfEmptyFields];
+  };
+
+  const restart = () => {
+    const newStatus = [...currentStatus];
+    newStatus.fill("");
     setStatus(newStatus);
+    setInstruction(startInstruction);
+  };
+
+  const displayEnd = (newStatus: Status[]) => {
+    if (determineWinner(newStatus)) setInstruction(endInstruction);
+  };
+
+  const determineWinner = (newStatus: Status[]): Status => {
+    const [p] = getCurrentPlayer(newStatus);
+    const player = p === 'X' ? 'O' : 'X'
+    for (let i = 0; i < winConditions.length; i++) {
+      const condition = winConditions[i];
+      const isFullfilled = condition.every((fieldNumber) => {
+        return newStatus[fieldNumber] === player;
+      })
+      console.log("condition", i, isFullfilled)
+      if (isFullfilled) return player;
+    }
+    return ""
   };
 
   return (
@@ -80,9 +132,8 @@ export const TicTacToe = () => {
           );
         })}
       </View>
-      <Text style={styles.anleitung}>
-        X fängt an. Tippe ein Feld zum Starten an.
-      </Text>
+      <Text style={styles.anleitung}>{currentInstruction}</Text>
+      <Button text={"Restart Game"} action={restart}></Button>
     </>
   );
 };
