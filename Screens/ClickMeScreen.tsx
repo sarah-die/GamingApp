da6501d4aead607817ed40b93be7110a28db1f93
sarah-die <sarah.diethert@gmail.com>
2022-08-13 15:12:55 +0200

    prettify

--- Screens/ClickMeScreen.tsx
+++ Screens/ClickMeScreen.tsx
@@ -1,12 +1,12 @@
 import * as React from "react";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { RootStackParamList } from "../Navigation";
-import {ClickMe} from "../Games/ClickMe";
+import { ClickMe } from "../Games/ClickMe";
 
 type ClickMeScreenProps = NativeStackScreenProps<
-    RootStackParamList,
-    "ClickMeScreen"
-    >;
+  RootStackParamList,
+  "ClickMeScreen"
+>;
 
 export const ClickMeScreen = ({ navigation }: ClickMeScreenProps) => {
   return <ClickMe />;
