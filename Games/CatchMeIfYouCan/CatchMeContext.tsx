7620357909476c2bfcff167fc8dea1e42265e268
sarah-die <saridi@alice.de>
2023-04-27 14:53:33 +0200

    Added numberOfClicks-Status to the context to make it possible to change it.

--- Games/CatchMeIfYouCan/CatchMeContext.tsx
+++ Games/CatchMeIfYouCan/CatchMeContext.tsx
@@ -5,17 +5,24 @@ export type gameStatusType = "instruction" | "ingame" | "over";
 type contextType = {
   gameStatus: gameStatusType;
   setGameStatus: (g: gameStatusType) => void;
+  numberOfClicks: number;
+  setNumberOfClicks: (n: number) => void;
 };
 
 const CatchMeContext = createContext<contextType>({
   gameStatus: "instruction",
   setGameStatus: () => {},
+  numberOfClicks: 10,
+  setNumberOfClicks: () => {},
 });
 
 export const CatchMeProvider = (props: { children: React.ReactNode }) => {
   const [gameStatus, setGameStatus] = useState<gameStatusType>("instruction");
+  const [numberOfClicks, setNumberOfClicks] = useState<number>(10);
   return (
-    <CatchMeContext.Provider value={{ gameStatus, setGameStatus }}>
+    <CatchMeContext.Provider
+      value={{ gameStatus, setGameStatus, numberOfClicks, setNumberOfClicks }}
+    >
       {props.children}
     </CatchMeContext.Provider>
   );
