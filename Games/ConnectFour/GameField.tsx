01ef12ad41d9aff0073766ba70137813335273cb
sarah-die <saridi@alice.de>
2023-04-27 11:09:58 +0200

    Fixed bugs: Fields are no longer clickable when the game is over If the game ends with a draw the players are now informed about it

--- Games/ConnectFour/GameField.tsx
+++ Games/ConnectFour/GameField.tsx
@@ -1,4 +1,4 @@
-import { Text, TouchableOpacity, View } from "react-native";
+import { Alert, Text, TouchableOpacity, View } from "react-native";
 import { Button } from "../../Elemente/Button";
 import { styles } from "./Styles";
 import { useConnectFourContext } from "./ConnectFourContext";
@@ -20,6 +20,7 @@ export const GameField = () => {
     new Array(42).fill("")
   );
 
+  // determines the current player depending on the currentFieldStatus
   const currentPlayer = useMemo(() => {
     const emptyFields = currentFieldStatus.filter((el) => el === "").length;
     // if (emptyFields % 2 === 0) {
@@ -30,6 +31,7 @@ export const GameField = () => {
     return emptyFields % 2 ? "B" : "A";
   }, [currentFieldStatus]);
 
+  // changes the current displayed instruction depending on the currentFieldStatus
   const currentInstruction = useMemo(() => {
     const startInstruction =
       "Player A begins. Choose a column to place your first token.";
@@ -37,34 +39,26 @@ export const GameField = () => {
       "Player " +
       currentPlayer +
       "'s turn. Choose a column to place your token.";
-    const endInstruction = "The game is over. Click the button to start a new game.";
+    const endInstruction =
+      "The game is over. Click the button to start a new game.";
 
     if (ctx.currentGameStatus === "active") {
-      const emptyFields = currentFieldStatus.filter((el) => el === "").length;
+      const emptyFields: number = currentFieldStatus.filter(
+        (el) => el === ""
+      ).length;
       if (emptyFields === 42) {
         return startInstruction;
-      } else if (emptyFields % 2 === 0) {
-        return playerInstruction;
       } else {
         return playerInstruction;
       }
     } else {
       return endInstruction;
     }
-
-    const emptyFields = currentFieldStatus.filter((el) => el === "").length;
-    if (emptyFields === 42) {
-      return startInstruction;
-    } else if (emptyFields % 2 === 0) {
-      return playerInstruction;
-    } else {
-      return playerInstruction;
-    }
   }, [currentFieldStatus]);
 
+  // places the new token in the clicked column and evaluates if the game is over
   const placeToken = (colIndex: number) => {
     // Range der Felder: 1 + colIndex * 6 bis 6 + colIndex * 6
-
     const newFieldStatus = currentFieldStatus.slice();
 
     let rowIndex = 5;
@@ -83,18 +77,25 @@ export const GameField = () => {
       }
     }
 
-    setFieldStatus(newFieldStatus);
-
+    if (newFieldStatus.every((el) => el !== "")) {
+      ctx.setGameStatus("over");
+      Alert.alert("Game Over", "The Game is over. You both played strong. The game is a draw.");
+    }
     if (checkForWinner(newFieldStatus, colIndex, rowIndex)) {
       ctx.setGameStatus("over");
-      alert("The Game is over. Congratulations to Player " + currentPlayer + "!");
+      Alert.alert("Game Over",
+        "The Game is over. Congratulations to Player " + currentPlayer + "!"
+      );
     }
+
+    setFieldStatus(newFieldStatus);
   };
 
+  // set all values to initial
   const restart = () => {
-    ctx.setGameStatus("active")
-    ctx.setColorPickerStatus("off")
-    ctx.setStatus("pickColor")
+    ctx.setGameStatus("active");
+    ctx.setColorPickerStatus("off");
+    ctx.setStatus("pickColor");
   };
 
   return (
@@ -132,7 +133,7 @@ export const GameField = () => {
           title={"Restart"}
           onPress={() => restart()}
           style={[styles.generalButton]}
-          textStyle={[styles.buttonText, { fontWeight: "bold" }]}
+          textStyle={[styles.buttonText]}
         ></Button>
       </View>
     </>
