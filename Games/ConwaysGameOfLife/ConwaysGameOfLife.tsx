5936f328a383a6a51e9027c7e7e302a7c70e28c6
sarah-die <saridi@alice.de>
2023-04-28 11:50:13 +0200

    Added Conways Provider and moved all screens into a new component called GameScreens

--- Games/ConwaysGameOfLife/ConwaysGameOfLife.tsx
+++ Games/ConwaysGameOfLife/ConwaysGameOfLife.tsx
@@ -1,14 +1,24 @@
 // Hier soll mit Hilfe eines States bestimmt werden, welcher Screen angezeigt wird
-import { useConwaysContext } from "./ConwaysContext";
+import { ConwaysProvider, useConwaysContext } from "./ConwaysContext";
 import { ConwaysGameFieldScreen } from "./ConwaysGameFieldScreen";
 
-export const ConwaysGameOfLife = () => {
+export const ConwaysGameOfLife = () => (
+  <ConwaysProvider>
+    <GameScreens />
+  </ConwaysProvider>
+);
+
+export const GameScreens = () => {
   const ctx = useConwaysContext();
-  if (ctx.gameStatus === "dimensions") {
-    // return <ConwaysDimensionScreen />
-  } else if (ctx.gameStatus === "placement") {
-    // return <ConwaysPlacementSceen />
-  } else {
-    return <ConwaysGameFieldScreen />;
-  }
+
+  // ToDo: andere Screens ergänzen
+  // if (ctx.gameStatus === "dimensions") {
+  //   // return <ConwaysDimensionScreen />
+  // } else if (ctx.gameStatus === "placement") {
+  //   // return <ConwaysPlacementSceen />
+  // } else {
+  //   return <ConwaysGameFieldScreen />;
+  // }
+
+  return <ConwaysGameFieldScreen />;
 };
